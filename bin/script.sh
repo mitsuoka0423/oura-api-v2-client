47914b6e035326a9d75d6fbc80fa53adae9c7dbb
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-07-31 03:09:38 +0900

    upd

--- bin/script.sh
+++ bin/script.sh
@@ -1,2 +1,2 @@
-sed -ie 's/exports.default/module.exports/' ./dist/cjs/index.js
-sed -ie 's/export default/export =/' ./dist/cjs/index.d.ts
+sed -ie 's/exports.default/module.exports/' ./dist/cjs/src/index.js
+sed -ie 's/export default/export =/' ./dist/cjs/src/index.d.ts
